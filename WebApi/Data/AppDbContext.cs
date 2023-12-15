99de7f4b507fe7b8e466bbedc6129efe0e9a5396
b.kabylov <b.kabylov>
2023-12-15 15:47:44 +0600

    Initial

---
+++ WebApi/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace WebApi.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+		}
+	}
+}
